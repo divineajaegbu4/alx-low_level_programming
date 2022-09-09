e5fd3f38a7167fc4fd12dc7bf40651a1464713f7
Ajaegbu Divine <divineajaegbu4@gmail.com>
2022-09-09 09:03:17 -0700

    Task 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ *  main - Entry point
  * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char test_str[] = "\"with proper grammar, but the outcome is a piece of art,";
-
-        print(test_str);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
