8dc113f357fe5940512ab7ae4e4906d8653409a2
Ajaegbu Divine <divineajaegbu4@gmail.com>
2022-09-09 08:09:19 -0700

    Task 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
- *    
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		char test_str[] = "\"Programming is like building a multilingual puzzle";
+
+			puts(test_str);
+				return (0);
 }
