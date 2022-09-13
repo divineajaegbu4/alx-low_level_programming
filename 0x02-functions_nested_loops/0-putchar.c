3845fd1d10d99c3e6e6de50bf712118b671abce7
Ajaebgu Divine <divineajaegbu4@gmail.com>
2022-09-13 12:56:34 +0100

    Task  0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
 #include "main.h"
 /**
- * main - Entry point
+ * main - Prints Holberton
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char *sh = "_putchar";
-
-        while (*sh)
-        {
-                _putchar(*sh);
-                sh++;
-        }
-        _putchar('\n');
-
-        return (0);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
 }
-
