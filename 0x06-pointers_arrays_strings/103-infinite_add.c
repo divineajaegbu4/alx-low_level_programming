52f173af75fe5035c2f3c5d630b520caaecbfe89
Ajaebgu Divine <divineajaegbu4@gmail.com>
2022-09-22 09:57:59 +0100

    Fix code

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,59 +1,86 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ *_strlen - counts and reutns string length
+ * @s: the input string
  *
- * Return: Always 0.
+ * Return: the length
  */
-int main(void)
+int _strlen(char *s)
 {
-        char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
-        char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
-        char r[100];
-        char r2[10];
-        char r3[11];
-        char *res;
+	int c = 0;
 
-        res = infinite_add(n, m, r, 100);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        n = "1234567890";
-        m = "1";
-        res = infinite_add(n, m, r2, 10);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        n = "999999999";
-        m = "1";
-        res = infinite_add(n, m, r2, 10);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        res = infinite_add(n, m, r3, 11);
-        if (res == 0)
-        {
-                printf("Error\n");
-        }
-        else
-        {
-                printf("%s + %s = %s\n", n, m, res);
-        }
-        return (0);
+	for (c = 0; *s; c++)
+		s++;
+
+	return (c);
+}
+
+
+/**
+ * rev_string - reverses a string
+ * @s: the address of the input string
+ *
+ * Return: nothing to return
+ */
+
+void rev_string(char *s)
+{
+	int i, sl;
+	char *a, *z, tp;
+
+	sl = _strlen(s);
+	a = s;
+	z = s;
+
+	for (i = 0; i < sl - 1; i++)
+		z++;
+
+	for (i = 0; i < (sl / 2); i++)
+	{
+		tp = *z;
+		*z = *a;
+		*a = tp;
+		a++;
+		z--;
+	}
+}
+
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: the buffer that the function will use to store the result
+ * @size_r: buffer size
+ *
+ * Return: pointer to the result, or 0.
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+	char *bfr = r;
+	int idx = 0, n1l = _strlen(n1), n2l = _strlen(n2), rdr = 0;
+	int p1, p2, sum;
+
+	while (n1l > 0 || n2l > 0)
+	{
+		p1 = n1l > 0 ? (*(n1 + n1l - 1) - '0') : 0;
+		p2 = n2l > 0 ? (*(n2 + n2l - 1) - '0') : 0;
+		sum = p1 + p2;
+		*(bfr + idx) = ((sum + rdr) % 10) + '0';
+		rdr = (sum + rdr) / 10;
+		idx++;
+		n1l--;
+		n2l--;
+	}
+	if (rdr)
+	{
+		*(bfr + idx) = rdr + '0';
+		idx++;
+	}
+	*(bfr + idx) = '\0';
+	rev_string(bfr);
+	return ((size_r > _strlen(bfr)) ? bfr : 0);
 }
