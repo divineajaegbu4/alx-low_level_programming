e46383313594a68a64c94487b6c8a886b230807c
Ajaebgu Divine <divineajaegbu4@gmail.com>
2022-09-30 15:02:02 +0100

    Task args

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
-#include "main.h"
 /**
- * main - prints its name + \n
- * @argc: number of arguments typed
- * @argv: array pointing to arguements
- * Return: 0
+ * main - program startup
+ * @argc: argument count
+ * @argv: argument vector
+(*
+ * Description: prints its own program name followed by a new line)?
+ * Return: return (0) is the required function signature
  */
+
+#include "main.h"
 int main(int argc, char *argv[])
 {
-	argc = 0;
-
-	printf("%s\n", argv[argc]);
+	(void) argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
